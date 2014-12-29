23aa5879656899c898672b5bb57d172ff21c4fe1
Andreas Linz <klingt.net@gmail.com>
2014-12-29 16:45:32 +0100

    removed pathlib from tools.py

--- sblgntparser/tools.py
+++ sblgntparser/tools.py
@@ -1,27 +1,22 @@
 import json
 import os
+import os.path
 
 import logging
 log = logging.getLogger(__name__)
 
-from pathlib import Path
-
-def filelist(folderpath, ext=None, flat=True):
+def filelist(folderpath, ext=None):
     '''
     Returns a list of all the files contained in the folder specified by `folderpath`.
     To filter the files by extension simply add a list containing all the extension with `.` as the second argument.
     If `flat` is False, then the Path objects are returned.
     '''
-    path = Path(folderpath)
     if not ext:
         ext = []
-    if path.exists() and path.is_dir():
-        if flat:
-            return [ str(f) for f in path.iterdir() if f.is_file() and f.suffix in ext ]
-        else:
-            return [ f for f in path.iterdir() if f.is_file() and f.suffix in ext ]
+    if os.path.exists(folderpath) and os.path.isdir(folderpath):
+        return [ os.path.join(folderpath, f) for f in os.listdir(folderpath) if os.path.isfile(f) and os.path.splitext(f)[1] in ext ]
     else:
-        log.warn('Nothing found in "{}"'.format(str(folderpath)))
+        log.warn('"{}" does not exist or is not a directory'.format(folderpath))
 
 def particles(category=None):
     '''
