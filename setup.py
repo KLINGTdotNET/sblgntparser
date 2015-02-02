535931a833203e5d9065072ec988c575b493d67f
Andreas Linz <klingt.net@gmail.com>
2015-02-02 15:08:08 +0100

    increased version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def load_readme():
 # http://pythonhosted.org/setuptools/setuptools.html#developer-s-guide
 setup(
     name = "sblgntparser",
-    version = "0.4.2",
+    version = "0.4.3",
     packages = ["sblgntparser"],
 
     install_requires = [],
